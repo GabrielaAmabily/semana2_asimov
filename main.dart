8d4c6f180315dec4a3bf3432467daec4557fac92
Gaby <amabilygabriela56@gmail.com>
2025-12-15 07:46:33 -0300

    criando as classes

--- main.dart
+++ main.dart
@@ -1,8 +1,27 @@
+//classe base
+class Item {
+  String titulo;
+  double anoPubli;
+  double quantCopias;
+  double precoBase;
+  double multaDia;
+}
+
+//classe livro, puxa de item
+class Livro extends Item {
+  String autor;
+  double isbn;
+}
+
+class Revista extends Item {
+  double edicao;
+  String mes; 
+}
+
 void main() {
   print("Olá, Seta! Dart está funcionando :)");
 
-    int a = 10;
-    int b = 15;
-  print (a + b);
-
+  int a = 10;
+  int b = 15;
+  print(a + b);
 }
