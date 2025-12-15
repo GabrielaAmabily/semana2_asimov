cb66fbcc44ece3549456655ae27ee32f0c665c48
Gaby <amabilygabriela56@gmail.com>
2025-12-15 16:27:57 -0300

    implementando metodos para emprestar e devolver

--- main.dart
+++ main.dart
@@ -5,6 +5,9 @@ class ItemBiblioteca {
   int quantCopias;
   double precoBase;
   double multaDia;
+  DateTime? dataEmprestimo; /* garante que o campo dataEmprestimo so possa 
+  armazenar valores válidos de data e hora, o que é essencial para calcular a multa */
+
 
   ItemBiblioteca(
     this.titulo,
@@ -13,6 +16,31 @@ class ItemBiblioteca {
     this.precoBase,
     this.multaDia,
   );
+
+  //metodo para emprestar
+  bool emprestar() {
+    if (quantCopias > 0) {
+      quantCopias--;
+      dataEmprestimo = DateTime.now();
+      print('o item {$titulo} foi emprestado com sucesso!');
+      return true;
+    } else {
+      print('o item não está disponivel.');
+      return false;
+    }
+  }
+
+  //metodo para devolver
+  double devolver(DateTime dataDevolucao) {
+    int atraso = 0;
+    quantCopias++;
+    int diasEmprestados = dataDevolucao.difference(dataEmprestimo!).inDays;
+    int diasPermitidos = 7; //chutei esse valor
+    if (diasEmprestados > diasPermitidos)
+      atraso = diasEmprestados - diasPermitidos;
+    double valorTotal = precoBase + (atraso * multaDia);
+    return valorTotal;
+  }
 }
 
 class Livro extends ItemBiblioteca {
@@ -33,11 +61,29 @@ class Revista extends ItemBiblioteca {
 
 void main() {
   List<ItemBiblioteca> acervo = [
-    Livro('O Hobbit', 1937, 3, 'J.R.R. Tolkien', '123456'),
+          // nome  , ano , quant, autor , isbn
+    Livro('O Hobbit', 1937, 2, 'J.R.R. Tolkien', '123456'),
     Livro('1984', 1949, 2, 'George Orwell', '654321'),
     Livro('Dom Casmurro', 1899, 1, 'Machado de Assis', '987654'),
+              // nome  , ano , quant, edicao , mes
     Revista('Superinteressante', 2024, 5, 420, 'Janeiro'),
     Revista('Veja', 2024, 4, 1050, 'Fevereiro'),
     Revista('Galileu', 2024, 2, 380, 'Março'),
   ];
+
+
+  // Testando empréstimo
+   ItemBiblioteca livro1 = acervo[0];
+
+  livro1.emprestar();
+  livro1.emprestar();
+  livro1.emprestar(); // aqui deve dizer que não está disponível
+
+  // Simulando devolução após 10 dias
+  DateTime dataDevolucao = DateTime.now().add(Duration(days: 10));
+  double valor = livro1.devolver(dataDevolucao);
+
+  print('Valor a pagar: R\$ ${valor.toStringAsFixed(2)}');
 }
+
+
